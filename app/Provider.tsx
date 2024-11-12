86fd60db477cd4210342253a12f1a34768885739
Marcelo Melogno <marcelomelogno1986@gmail.com>
2024-11-11 21:17:37 -0300

    Implement mention feature

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,21 +1,35 @@
 'use client'
 
 import Loader from "@/components/Loader"
-import { getClerkUser } from "@/lib/actions/user.actions"
+import { getClerkUser, getDocumentUsers } from "@/lib/actions/user.actions"
+import {  useUser } from "@clerk/nextjs"
 import { ClientSideSuspense, LiveblocksProvider, RoomProvider } from "@liveblocks/react/suspense"
 import { ReactNode } from "react"
 
 
+
 const Provider = ({ children }: { children: ReactNode }) => {
+	const { user: clerkUser } = useUser()
 	return (
-		<LiveblocksProvider authEndpoint={'/api/liveblocks-auth'} 
-		resolveUsers={async ({userIds})=>{
-			const users = await getClerkUser({userIds})
-			console.log('users in Provider', users)
-			return users
-		}}>
+		<LiveblocksProvider authEndpoint={'/api/liveblocks-auth'}
+			resolveUsers={async ({ userIds }) => {
+				const users = await getClerkUser({ userIds })
+				console.log('users in Provider', users)
+				return users
+			}}
+			resolveMentionSuggestions={async ({ text, roomId }) => {
+				const roomUsers = await getDocumentUsers({
+				  roomId,
+				  // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
+				  currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+				  text,
+				})
+		
+				return roomUsers;
+			  }}
+		>
 			<RoomProvider id="my-room">
-				<ClientSideSuspense fallback={<Loader/>}>
+				<ClientSideSuspense fallback={<Loader />}>
 					{children}
 				</ClientSideSuspense>
 			</RoomProvider>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,15 +1,16 @@
 "use server";
 
-import {clerkClient} from '@clerk/clerk-sdk-node'
+import { clerkClient } from "@clerk/clerk-sdk-node";
 import { parseStringify } from "../utils";
+import { liveblocks } from "../liveblocks";
 
 export const getClerkUser = async ({ userIds }: { userIds: string[] }) => {
   try {
-    console.log('userIds: ', userIds)
+    console.log("userIds: ", userIds);
     const { data } = await clerkClient.users.getUserList({
       emailAddress: userIds,
     });
-    console.log('data:', data)
+    console.log("data:", data);
     // console.log('data in getClerckUser', data)
     const users = data.map((user) => ({
       id: user.id,
@@ -22,8 +23,39 @@ export const getClerkUser = async ({ userIds }: { userIds: string[] }) => {
       users.find((user) => user.email === email)
     );
 
-    return parseStringify(sortedUsers)
+    return parseStringify(sortedUsers);
   } catch (error) {
     console.log("error in getClerkUser: ", error);
+  }
+};
+
+export const getDocumentUsers = async ({
+  roomId,
+  currentUser,
+  text,
+}: {
+  roomId: string;
+  currentUser: string;
+  text: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    const users = Object.keys(room.usersAccesses).filter(
+      (email) => email !== currentUser
+    );
+
+    if (text.length > 0) {
+      const lowerCaseText = text.toLowerCase();
+
+      const filteredUsers = users.filter((email) =>
+        email.toLowerCase().includes(lowerCaseText)
+      );
+
+      return parseStringify(filteredUsers);
+    }
+    return parseStringify(users);
+  } catch (error) {
+    console.log("error in getDocumentsUsers: ", error);
   }
 };
